f6983624683477d3e917c9e7c45d2b90f8b515c7
Amanda Portela <mandaptela@gmail.com>
2022-03-31 20:18:25 -0300

    requisito 3 ok

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// Requisito 3 - terminar
-// Ao preencher o formulário e clicar no botão, será validado que:
-// Caso o email seja "tryber@teste.com" e a senha seja "123456" será emitido um alerta contendo o texto "Olá, Tryber!"
-// Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos."
-/* Ao clicar no botão de login dispara um alert com o texto "Email ou senha inválidos", no caso de erro de preenchimento dos dados. Ao clicar no botão de login dispara um alert com o texto "Olá, Tryber!", no caso de preenchimento correto dos dados. */
+const botaoLogin = document.querySelector('.botao-login');
+const email = document.querySelector('#login-email');
+const senha = document.querySelector('#login-senha');
+function login() {
+    if (email.value === 'tryber@teste.com' && senha.value === '123456'){
+        alert('Olá, Tryber!');
+    }
+    else{
+        alert('Email ou senha inválidos.');
+    }
+}
+botaoLogin.addEventListener('click', login);
