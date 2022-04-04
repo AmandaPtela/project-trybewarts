69928aeb22cdea4d5055db1ed9a259141cab4b96
Amanda Portela <mandaptela@gmail.com>
2022-04-04 18:50:39 -0300

    .

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const senha = document.querySelector('#login-senha');
 const botaoEnviar = document.getElementById('submit-btn');
 const check = document.getElementById('agreement');
 
-const login = () =>  email.value === 'tryber@teste.com' && senha.value === '123456' ? alert('Olá, Tryber!') : alert ('Email ou senha inválidos.');
+const login=() =>
+email.value === 'tryber@teste.com' && senha.value === '123456' ? alert('Olá, Tryber!') : alert ('Email ou senha inválidos.');
 botaoLogin.addEventListener('click', login);
 
-let agreeButton = () => check.ariaChecked ? botaoEnviar.disabled = true : botaoEnviar.disabled = false;
+const agreeButton=() =>
+check.ariaChecked ? botaoEnviar.disabled = true : botaoEnviar.disabled = false;
 check.addEventListener('click', agreeButton);
